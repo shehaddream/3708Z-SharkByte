50b412670de5d4fb1d4e9a63379784d63002a5c7
tiffany <tiffanymeowcat@gmail.com>
2025-10-17 13:41:35 +0800

    -Tiffany

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include "main.h"
 
-/////
-// For installation, upgrading, documentations, and tutorials, check out our website!
-// https://ez-robotics.github.io/EZ-Template/
-/////
 
 // Chassis constructor
 ez::Drive chassis(
